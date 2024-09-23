6e98e2c44114e2a07205d919d729d89493ed7c1d
ShorgoShiam <shorgoshiam@gmail.com>
2024-09-23 23:30:32 +0600

    second commit

---
+++ js/home.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const AddMoneyInput = document.getElementById('input-field').value;
+    const pinNumber = document.getElementById('input-pin').value
+})
--- js/login.js
+++ js/login.js
@@ -1,6 +1,13 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value
+    if (phoneNumber === "01747355949" && pinNumber === "1234"){
+        console.log('WOW');
+        window.location.href = '/home.html'
+    }
+    else{
+        alert("Wrong information!")
+    }
 
 })
